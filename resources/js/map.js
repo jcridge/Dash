8e94880f968263a77ed955f15050af2ac0765f8f
jcridge <crij1_12@uni.worc.ac.uk>
2015-04-21 16:36:11 +0000

    Added timer
    When clicking the run button, a timer begins. Code from:
    http://jsfiddle.net/Daniel_Hug/pvk6p/

--- resources/js/map.js
+++ resources/js/map.js
@@ -47,4 +47,41 @@ function startGeo(){
     // Browser doesn't support Geolocation
     handleNoGeolocation(false);
   }
+
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    h6.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
 }
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
+
+
+/* Start button */
+start.onclick = timer;
+
+/* Stop button */
+stop.onclick = function() {
+    clearTimeout(t);
+}
+ 
+}
+
+var h6 = document.getElementsByTagName('h6')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
