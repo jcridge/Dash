bb142cf22cab27f62794b2831f9a430a5dc0f173
jcridge <crij1_12@uni.worc.ac.uk>
2015-04-21 17:42:14 +0000

    Trialling track functionality
    http://stackoverflow.com/questions/3305225/real-time-gps-tracker-on-just-html-js-and-google-maps-to-be-run-on-a-handphone

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,10 +1,17 @@
+
+var h6 = document.getElementsByTagName('h6')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stopRun'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
 var map;
 
 function initialize() {
   var mapOptions = {
-    zoom: 14,
+    zoom: 16,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    center: new google.maps.LatLng(51.5000, 0.1167),
+    disableDefaultUI: true
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
@@ -31,11 +38,34 @@ function startGeo(){
       var pos = new google.maps.LatLng(position.coords.latitude,
                                        position.coords.longitude);
 
-    var marker = new google.maps.Marker({
-      position: pos,
-      map: map
-    });
+var marker = null;
 
+function autoUpdate() {
+  navigator.geolocation.getCurrentPosition(function(position) {  
+    var newPoint = new google.maps.LatLng(position.coords.latitude, 
+                                          position.coords.longitude);
+
+    if (marker) {
+      // Marker already created - Move it
+      marker.setPosition(newPoint);
+    }
+    else {
+      // Marker does not exist - Create it
+      marker = new google.maps.Marker({
+        position: newPoint,
+        map: map
+      });
+    }
+
+    // Center the map on the new position
+    map.setCenter(newPoint);
+  }); 
+
+  // Call the autoUpdate() function every 5 seconds
+  setTimeout(autoUpdate, 5000);
+}
+
+autoUpdate(); 
       map.setCenter(pos);
     }, function() {
       handleNoGeolocation(true);
@@ -47,6 +77,8 @@ function startGeo(){
   }
 
 
+ 
+
 function add() {
     seconds++;
     if (seconds >= 60) {
@@ -57,29 +89,21 @@ function add() {
             hours++;
         }
     }
-    
     h6.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-
     timer();
-}
-function timer() {
-    t = setTimeout(add, 1000);
-}
-timer();
+  }
+  
+  function timer() {
+      t = setTimeout(add, 1000);
+  }
+  
+  timer();
 
+  /* Start button */
+  start.onclick = timer;
 
-/* Start button */
-start.onclick = timer;
-
-/* Stop button */
-stop.onclick = function() {
-    clearTimeout(t);
+  /* Stop button */
+  stop.onclick = function() {
+      clearTimeout(t);
+  }
 }
- 
-}
-
-var h6 = document.getElementsByTagName('h6')[0],
-    start = document.getElementById('start'),
-    stop = document.getElementById('stopRun'),
-    seconds = 0, minutes = 0, hours = 0,
-    t;
